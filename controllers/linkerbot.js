17f4ed2cbd968f57ebc05a629e1ca0e77d27a6a4
sumukshashidhar <sumukuuu@gmail.com>
2020-05-14 13:27:41 +0530

    simple parsing

--- controllers/linkerbot.js
+++ controllers/linkerbot.js
@@ -2,6 +2,16 @@ module.exports = {
 
 
     checkMessage: function checkMessage(message) {
-        return true;
+        var strs = '';
+        for(let i=0; i<message.length; i++) {
+            if(message[i] == '{') {
+                let j = i; 
+                while (message[j] != '}') {
+                    strs = strs + message[j];
+                    j = j + 1;
+                }
+                return strs;
+            }
+        }
     }
 }
