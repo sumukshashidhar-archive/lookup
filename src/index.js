0b4d846e3ba16efdf621f6f9de8c630805aeff30
sumukshashidhar <sumukuuu@gmail.com>
2020-05-14 13:06:31 +0530

    added the linkerbot and basic chat

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 var express = require("express");
-
-
 var app = express();
 
-app.listen(3000, process.env.IP, function (req,res) {
-    console.log("SERVER STARTED");
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', function(req, res) {
+    console.log('hit the home page');
+    res.render('index.ejs')
+})
+
+io.sockets.on('connection', function(socket) {
+    // write all the realtime communication functions here
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + 'has joined the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + 'has left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        //have to parse this message first
+        console.log(message)
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
 });
 
 
-app.get('/works', function(req, res) {
-    console.log('hit the works page');
-    res.json('server runs')
-})
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
