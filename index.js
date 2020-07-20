c9255e7a81fcf444fe0e4b412a1bccf3cd547075
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-07-20 07:53:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ io.sockets.on('connection', function(socket) {
 });
 
 
-const server = http.listen(8080, function() {
+const server = http.listen(env.PORT, function() {
     console.log('listening on *:8080');
 });
